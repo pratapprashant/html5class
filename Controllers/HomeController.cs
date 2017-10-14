1dce26497f867668afa19bd15cdef95315640972
Prashant Pratap <prashantpratap@msn.com>
2017-10-14 15:36:42 -0500

    init

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using mvccore.Models;
+
+namespace mvccore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            ViewBag.MyTime =  DateTime.Now;
+
+            return View();
+        }
+
+        public IActionResult Contact2(string id)
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            ViewBag.MyID =  id;
+
+            return View("Contact");
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
